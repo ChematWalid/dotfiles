7e1194f3e48aca481af6fa971a505818c7f29b44
Your Name <chemat_oualid@univ-blida.dz>
2026-08-20 02:22:52 +0100

    Enable Tab / Shift-Tab and Enter completion in Neovim alongside Primeagen keys

--- .config/nvim/lua/theprimeagen/lazy/lsp.lua
+++ .config/nvim/lua/theprimeagen/lazy/lsp.lua
@@ -59,17 +59,37 @@ return {
 
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
+        local luasnip = require('luasnip')
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
+                    luasnip.lsp_expand(args.body)
                 end,
             },
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
